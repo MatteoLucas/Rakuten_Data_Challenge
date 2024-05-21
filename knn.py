22227d9756c2f15e0048f82eb81f509d91420db1
Matteo Lucas <matteo.lucas@student-cs.fr>
2024-05-21 20:19:26 +0100

    Implémentation de la méthode knn, f1_score = 0,7088

---
+++ knn.py
@@ -0,0 +1,25 @@
+def knn_train(teacher_mode):
+    """Entraine le modèle KNN avec validation croisée et le sauvegarde"""
+    from sklearn.neighbors import KNeighborsClassifier
+    from sklearn.model_selection import GridSearchCV
+    import TextProcessing as TP
+    
+    # Récupérer les données d'entraînement et de test
+    X_train, X_test, Y_train, Y_test = TP.get_X_Y(teacher_mode)
+    
+    # Définition des paramètres pour la validation croisée
+    best_param = {'n_neighbors': 35,'weights': 'uniform','metric':'euclidean'} #Trouvés par validation croisée
+    
+    # Initialisation du modèle KNN
+    knn = KNeighborsClassifier(n_neighbors=best_param['n_neighbors'], weights=best_param['weights'], metric=best_param['metric'])
+        
+    # Entraîner le modèle avec les données d'entraînement
+    knn.fit(X_train, Y_train)
+    
+    # Sauvegarder le modèle entraîné
+    TP.save_model([knn, X_train, X_test, Y_train, Y_test], 'knn', teacher_mode)
+    
+    print("Modèle entraîné et sauvegardé avec succès.")
+
+if __name__=="__main__":
+    knn_train(True)
